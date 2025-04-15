e6ffaa65e74487ca0bdda252f89efd16a0dac038
muthiazzh <muthiahazzahra200@gmail.com>
2025-04-15 08:04:47 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8
+{
+    internal class BankTransferConfig
+    {
+        public BankTransfer bankTransfer;
+        public string filePath = @"bank_transfer_config.json";
+
+        public class BankTransfer
+        {
+            public string lang { get; set; }
+            public Transfer transfer { get; set; }
+            public List<string> methods { get; set; }
+            public Confirmation confirmation { get; set; }
+
+            public BankTransfer(string lang, Transfer transfer, List<string> methods, Confirmation confirmation)
+            {
+                this.lang = lang;
+                this.transfer = transfer;
+                this.methods = methods;
+                this.confirmation = confirmation;
+            }
+        }
+
+        public class Transfer
+        {
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+
+            public Transfer(int thres, int low, int high) 
+            { 
+                this.threshold = thres;
+                this.low_fee = low;
+                this.high_fee = high;
+            }
+        }
+
+        public class Confirmation
+        {
+            public string en { get; set; }
+            public string id { get; set; }
+
+            public Confirmation(string en, string id)
+            {
+                this.en = en;
+                this.id = id;
+            }
+        }
+
+        public BankTransferConfig()
+        {
+            try
+            {
+                readConfigFile();
+            }
+            catch
+            {
+                setDefault();
+                writeConfigFile();
+            }
+        }
+
+        private void setDefault()
+        {
+            bankTransfer = new BankTransfer(
+                "en", 
+                new Transfer(25000000, 6500, 15000), 
+                new List<string> { "RTO (real-time)", "SKN", "RTGS", "BI FAST" },
+                new Confirmation("yes", "ya")
+            );
+        }
+
+        private BankTransfer readConfigFile()
+        {
+            string json = File.ReadAllText(filePath);
+            bankTransfer = JsonSerializer.Deserialize<BankTransfer>(json);
+            return bankTransfer;
+        }
+
+        private void writeConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+
+            string jsonString = JsonSerializer.Serialize(bankTransfer, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,104 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul8;
+
+class main
+{
+    static BankTransferConfig config = new BankTransferConfig();
+    static string input;
+    static int nominalTransfer;
+    static int fee;
+    static string method;
+
+    public static void Main(string[] args)
+    {
+        menuTransfer();
+    }
+
+    public static void menuTransfer()
+    {
+        if (config.bankTransfer.lang.Equals("en"))
+        {
+            Console.WriteLine("Please insert the amount of money to transfer: ");
+        }
+        else
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer: ");
+        }
+
+        input = Console.ReadLine();
+
+        try
+        {
+            nominalTransfer = int.Parse(input);
+                
+            if (nominalTransfer <= config.bankTransfer.transfer.threshold)
+            {
+                fee = config.bankTransfer.transfer.low_fee;
+            }
+            else
+            {
+                fee = config.bankTransfer.transfer.high_fee;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("[Error]");
+            menuTransfer();
+        }
+        if (config.bankTransfer.lang.Equals("en"))
+        {
+            Console.WriteLine($"Transfer fee = {fee} dan Total = {nominalTransfer + fee}");
+            Console.WriteLine("Select transfer method: ");
+        }
+        else
+        {
+            Console.WriteLine($"Fee transfer = {fee} dan Total biaya = {nominalTransfer + fee}");
+            Console.WriteLine("Pilih metode transfer: ");
+        }
+
+        for (int i = 0; i < config.bankTransfer.methods.Count; i++)
+        {
+            Console.WriteLine($"{i + 1} {config.bankTransfer.methods.ElementAt(i)}");
+        }
+        input = Console.ReadLine();
+
+        try
+        {
+            method = config.bankTransfer.methods.ElementAt(int.Parse(input));
+        }
+        catch
+        {
+            Console.WriteLine("[Error]");
+            menuTransfer();
+        }
+
+        if (config.bankTransfer.lang.Equals("en"))
+        {
+            Console.WriteLine($"Please type {config.bankTransfer.confirmation.en} to confirm the transaction: ");
+            input = Console.ReadLine();
+            if (input == config.bankTransfer.confirmation.en)
+            {
+                Console.WriteLine("The transfer is completed");
+            }
+            else
+            {
+                Console.WriteLine("Transfer is cancelled");
+                menuTransfer();
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Ketik {config.bankTransfer.confirmation.id} untuk mengkonfirmasi transaksi: ");
+            input = Console.ReadLine();
+            if (input == config.bankTransfer.confirmation.id)
+            {
+                Console.WriteLine(" Proses transfer berhasil");
+            }
+            else
+            {
+                Console.WriteLine(" Transfer dibatalkan");
+                menuTransfer();
+            }
+        }
+
+    }
+}
